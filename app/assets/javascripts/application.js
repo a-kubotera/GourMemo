8e63707e66aa388f7f98d088a38df44c97c36fa6
a-kubotera <akira.kubotera+github@gmail.com>
2017-09-16 16:22:00 +0900

    修正
    2017/09/16　script動作確認し問題なかった

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require bootstrap-sprockets
+// [TOPIC] jquery_ujsはRemote:trueのときにauthenticity_tokenを送るために必須
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
