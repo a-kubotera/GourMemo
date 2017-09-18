0e413ae5f66505ee0319c33a6d0655bbd5323b47
a-kubotera <akira.kubotera+github@gmail.com>
2017-09-18 17:23:47 +0900

    UserEdit完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-  def profile_img(user)
+  def profile_img(user,avatarSize = "sizeDef")
+    return image_tag(user.avatar, alt: user.name,class:avatarSize) if user.avatar?
     unless user.provider.blank?
       img_url = user.image_url
     else
       img_url = 'no_image.png'
     end
-    image_tag(img_url, alt: user.name)
+    image_tag(img_url, alt: user.name,class:avatarSize)
   end
 end
