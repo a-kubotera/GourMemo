7ea1c8c3062929e866c876b3331450fbb637f218
a-kubotera <akira.kubotera+github@gmail.com>
2017-09-30 21:31:57 +0900

    評価の修正不具合対応

--- app/controllers/evaluates_controller.rb
+++ app/controllers/evaluates_controller.rb
@@ -37,12 +37,14 @@ class EvaluatesController < ApplicationController
   end
 
   def update
+
     respond_to do |format|
       if @evaluate.update(evaluate_params)
         format.html { redirect_to top_path, notice: '評価を修正しました！' }
         format.json { render :show, status: :ok, location: @evaluate }
       else
-        format.html { render :edit }
+        @article = Article.find(params[:article_id])
+        format.html { render :new }
         format.json { render json: @evaluate.errors, status: :unprocessable_entity }
       end
     end
