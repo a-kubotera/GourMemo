cea7a21ec22b859d16aa734a010daf244dd8284e
a-kubotera <akira.kubotera+github@gmail.com>
2018-06-14 18:14:47 +0900

    Fix Login

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,19 @@
-class Users::RegistrationsController  < Devise::RegistrationsController
-  #def build_resource(hash=nil)
-  #  hash[:uid] = User.create_unique_string
-  #  super
-  #end
+class Users::RegistrationsController < Devise::RegistrationsController
+  
+  # Device4.4.0のregistration_controllerのnewメソッドが
+  # build_resource となっており、その影響で undefined method `[]=' for nil:NilClass
+  # 修正のためにオーバーライド
+
+  def new
+    build_resource({})
+    yield resource if block_given?
+    respond_with resource    
+  end
+
+  def build_resource(hash=nil)
+    hash[:uid] = User.create_unique_string
+    super
+  end
 
   def edit
     @page_title = "マイページ(修正)"
